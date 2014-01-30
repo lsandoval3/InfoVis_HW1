0a4d13df16b8a1b29009e9bdfa2bf43b171aba97
lsandoval3 <les7uve@hotmail.com>
2014-01-30 17:36:24 -0500

    Graph updates based on X and/or Y axis changes

--- js/main.js
+++ js/main.js
@@ -2,81 +2,125 @@
 THIS FUNCTION IS CALLED WHEN THE WEB PAGE LOADS. PLACE YOUR CODE TO LOAD THE 
 DATA AND DRAW YOUR VISUALIZATION HERE. THE VIS SHOULD BE DRAWN INTO THE "VIS" 
 DIV ON THE PAGE.
-This function is passed the variables to initially draw on the x and y axes.
 **/
 
-var data,svg,x,y;
+/**
+Global variables
+**/
+var xLabel, yLabel;
 
-//Text to be displayed for the x and y axis
-var xAxis_label,yAxis_label;
+/**
+This function is passed the variables to initially draw graph
+**/
 
-
-xAxis_label="Kernel Length";
-yAxis_label="Kernel Width";
-
-function draw(){
-
+function draw(){	
+	
 	var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    	width = 960 - margin.left - margin.right,
-    	height = 500 - margin.top - margin.bottom;
-
+	    width = 960 - margin.left - margin.right,
+	    height = 550 - margin.top - margin.bottom;
+	
 	var x = d3.scale.linear()
-    	.range([0,width]);
-
+	    .range([0, width]);
+		
 	var y = d3.scale.linear()
-    	.range([height, 0]);
+		.range([height,0]);
+		
+	var color = d3.scale.category10();
+		
+	var	xAxis = d3.svg.axis()
+		    .scale(x)
+		    .orient("bottom")
+			.ticks(10,"");
 
-	var xAxis = d3.svg.axis()
-    	.scale(x)
-    	.orient("bottom")
-    	.ticks(10,"");
+	var	yAxis = d3.svg.axis()
+		    .scale(y)
+		    .orient("left")
+			.ticks(10,"");
+	
+	var canvas = d3.select('#vis')
+				.append("svg")
+				.attr("width", width + margin.left + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+				/**.style("background-color", "gray")**/
+				.append("g")
+				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		
+	d3.csv("data/data.csv", function(error, data) {
+		data.forEach(function(d) {
+			d.kernelLength = +d[xLabel];
+			d.kernelWidth = +d[yLabel];
+		});	
+		
+	    x.domain(d3.extent(data, function(d) { return d[xLabel]; })).nice();
+	    y.domain(d3.extent(data, function(d) { return d[yLabel]; })).nice();
+				
+		canvas.append("g")
+			.attr("class", "x axis")
+			.attr("transform", "translate(0," + height + ")")
+				.call(xAxis)
+			.append("text")
+			.attr("class", "label")
+			.attr("x", width)
+			.attr("y", -6)
+			.style("text-anchor", "end")
+			.text(xLabel);
 
-	var yAxis = d3.svg.axis()
-    	.scale(y)
-    	.orient("left")
-    	.ticks(10, "%");
+		canvas.append("g")
+			.attr("class", "y axis")
+				.call(yAxis)
+			.append("text")
+			.attr("class", "label")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 6)
+			.attr("dy", ".71em")
+			.style("text-anchor", "end")
+			.text(yLabel);
+			
+		 canvas.selectAll(".dot")
+		    .data(data)
+		    .enter().append("circle")
+		    .attr("class", "dot")
+		    .attr("r", 3.5)
+		    .attr("cx", function(d) { return x(d.kernelLength); })
+		    .attr("cy", function(d) { return y(d.kernelWidth); })
+		    .style("fill", function(d) { return color(d.variety); });
+			
+		 var legend = canvas.selectAll(".legend")
+		    .data(color.domain())
+		    .enter().append("g")
+		    .attr("class", "legend")
+		    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-	var svg = d3.select("#vis").append("svg")
-   		.attr("width", width + margin.left + margin.right)
-    	.attr("height", height + margin.top + margin.bottom)
-  		.append("g")
-    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		 legend.append("rect")
+		    .attr("x", width - 18)
+		    .attr("width", 18)
+		    .attr("height", 18)
+		    .style("fill", color);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-        .append("text")
-       	.attr("class", "label")
-       	.attr("x", width)
-       	.attr("y", -6)
-       	.style("text-anchor", "end")
-       	.text(xAxis_label);
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text(yAxis_label);
+		 legend.append("text")
+		    .attr("x", width - 24)
+		    .attr("y", 9)
+		    .attr("dy", ".35em")
+		    .style("text-anchor", "end")
+		    .text(function(d) { return d; });
+		
+	});	
+	
 }
 
-
-var text= function(d){
-		d.compactness = + d.compactness;
-	};
-
+/**
+var text = function(d){
+	d.compactness = +d.compactness;
+}
+**/
+/**
+This function is passed the variables to initially draw on the x and y axes.
+**/
 function init(xAxis, yAxis){
-	d3.csv("data/data.csv", text, function(error, data) {
-  		console.log(data.csv);
-  		data=data;
-  		x.domain(data.map(function(d) { return d.letter; }));
-  		y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
-  	});
+	xLabel = xAxis;
+	yLabel = yAxis;
 	draw();
+	
 }
 
 /**
@@ -85,12 +129,10 @@ This function is called whenever the menu for the variable to display on the
 x axis changes. It is passed the variable name that has been selected, such as
 "compactness". Populate this function to update the scatterplot accordingly.
 **/
-
 function onXAxisChange(value){
-	d3.select('svg').remove();
-	xAxis_label=value;
+	d3.select("svg").remove();
+	xLabel = value;
 	draw();
-
 }
 
 /**
@@ -101,8 +143,8 @@ y axis changes. It is passed the variable name that has been selected, such as
 accordingly.
 **/
 function onYAxisChange(value){
-	d3.select('svg').remove();
-	yAxis_label=value;
+	d3.select("svg").remove();
+	yLabel = value;
 	draw();
 }
 
